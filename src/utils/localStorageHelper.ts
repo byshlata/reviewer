2abccd137b3d395386651a0f1ce6fcebed390a2d
Aliaksandr <byshlata@yandex.ru>
2023-01-05 02:05:46 +0300

    delete console.log

--- src/utils/localStorageHelper.ts
+++ src/utils/localStorageHelper.ts
@@ -4,29 +4,26 @@ import { themeAppDark, themeAppLight } from 'theme';
 import { LanguageType, ThemeType, UndefindType } from 'types';
 
 export const loadStateApp = (): UndefindType<InitialStateAppType> => {
-  try {
-    const themeApp = localStorage.getItem('themeApp');
-    const languageApp = localStorage.getItem('languageApp');
-    const appInitialState: InitialStateAppType = {
-      errorMessage: '',
-      isProgress: false,
-      isAuthRequest: false,
-      themeApp: themeAppLight,
-      languageApp: Language.English,
-      appSettings: { tags: [], category: [] },
-    };
+  const themeApp = localStorage.getItem('themeApp');
+  const languageApp = localStorage.getItem('languageApp');
+  const appInitialState: InitialStateAppType = {
+    errorMessage: '',
+    isProgress: false,
+    isAuthRequest: false,
+    themeApp: themeAppLight,
+    languageApp: Language.English,
+    appSettings: { tags: [], category: [] },
+  };
 
-    if (JSON.parse(themeApp || '') === Theme.Dark) {
-      appInitialState.themeApp = themeAppDark;
-    }
+  if (JSON.parse(themeApp || '') === Theme.Dark) {
+    appInitialState.themeApp = themeAppDark;
+  }
 
-    if (JSON.parse(languageApp || '') === Language.Russian) {
-      appInitialState.languageApp = Language.Russian;
-    }
+  if (JSON.parse(languageApp || '') === Language.Russian) {
+    appInitialState.languageApp = Language.Russian;
+  }
 
-    return appInitialState;
-    // eslint-disable-next-line no-empty
-  } catch (err: any) {}
+  return appInitialState;
 };
 
 export const saveState = (themeApp: ThemeType, languageApp: LanguageType): void => {
